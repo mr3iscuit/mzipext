5f2ed8a002a10fbb5524754cbe94f7cb45c3d471
eyvaz <eyvaz.bayramov2018@gmail.com>
2025-09-28 09:29:27 +0400

    fix cmd name in mergeable command long usage.

--- cmd/cli/constants.go
+++ cmd/cli/constants.go
@@ -8,7 +8,7 @@ var (
 	MergeableCmdUse   = "mergeable"
 	MergeableCmdShort = "check if zip files are mergeable"
 	MergeableCmdLong  = `
-	gdextract mergeable <zip1> <zip2>
+	mzipext mergeable <zip1> <zip2>
 
 	checks if zip1 and zip2 are mergeable.
 	zip1 and zip2 must be valid zip files.
